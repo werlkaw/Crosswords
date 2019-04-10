e2ac37fe2abbc2576df3d029afbfb105e7106082
Juan Bustamante <jab675@cornell.edu>
2019-04-09 20:01:37 -0700

    add profile pic to side menu

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PathConstants } from 'src/app/constants/PathConstants';
+import { FirebaseUserService } from 'src/app/services/auth/firebase-user.service';
 
 @Component({
   selector: 'app-side-menu',
@@ -9,7 +10,7 @@ import { PathConstants } from 'src/app/constants/PathConstants';
 })
 export class SideMenuComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, public firebaseUser: FirebaseUserService) { }
 
   ngOnInit() {
   }
